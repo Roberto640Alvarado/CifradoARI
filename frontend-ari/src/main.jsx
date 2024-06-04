5be5f76f93ff05d0047f8c81a748c728b71d566c
EA03 <00175420@uca.edu.sv>
2024-06-03 19:27:20 -0600

    :lipstick:Agregar componente visualizacion de resultado

--- frontend-ari/src/main.jsx
+++ frontend-ari/src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '/src/App.jsx';
+import './index.css'; 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const jsonData = {
+    "documento": "03110567-7",
+    "nombres": "Jaime Roberto",
+    "apellidos": "Climaco Navarrete",
+    "tarjeta": "2346570012456",
+    "tipo": "GOLD",
+    "telefono": "987654321",
+    "poligono": {
+                  "type": "FeatureCollection",
+                  "properties": 
+                  {
+                    "tyoe": "name" ,
+                    "properties": 
+                    {
+                      "name": "EPSG:4326"
+                    }
+                  },
+                  "features": [
+                    
+                  ]
+    }
+
+};
+
+console.log('Rendering CardResult with jsonData:', jsonData);
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+        {/*<CardResult jsonData={jsonData} />*/}
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+console.log('ReactDOM.render called');
+
