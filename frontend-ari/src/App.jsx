bdcaf9019bd789302cba71160d0f328a0910acc4
omar554 <62614708+omar554@users.noreply.github.com>
2024-06-05 14:53:43 -0600

    Add: CardConvert

--- frontend-ari/src/App.jsx
+++ frontend-ari/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Card from '/src/components/Card/CardViewDataC'
+import CardConvert from '/src/components/Card/CardConvert'
 import './App.css'
 
 function App() {
