24bbc0102d52c4745bfbee3f3b14feb0d37b703a
omar554 <62614708+omar554@users.noreply.github.com>
2024-06-10 02:12:32 -0600

    🚀 Decryption implemented

--- frontend-ari/src/components/Card/CardDecryptC.jsx
+++ frontend-ari/src/components/Card/CardDecryptC.jsx
@@ -1,36 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import CifrarServices from "../../services/CifrarServices";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
-const DecryptCard = () =>{
+const MySwal = withReactContent(Swal);
+
+const DecryptCard = () => {
     const navigate = useNavigate();
-    const handleDecrypt = () => {
-        // Conversión de TXT a JSON utilizando key y delimiter
-        // Implementa tu lógica aquí
-        navigate('/dResultado');
+    const [fileContent, setFileContent] = useState(null);
+    const [key, setKey] = useState("");
+    const [delimiter, setDelimiter] = useState("");
+
+    const handleFileUpload = (event) => {
+        const file = event.target.files[0];
+        if (file && file.type !== "application/json") {
+            MySwal.fire({
+                icon: "error",
+                title: "Formato de archivo no válido",
+                text: "Por favor seleccione un archivo .json.",
+            });
+            return;
+        }
+        const reader = new FileReader();
+        reader.onload = (e) => setFileContent(JSON.parse(e.target.result));
+        reader.readAsText(file);
     };
+
+    const handleDecrypt = async () => {
+        if (!key || !delimiter || !fileContent) {
+            MySwal.fire({
+                icon: "error",
+                title: "Campos Obligatorios",
+                text: "Los campos de clave, delimitador y archivo son obligatorios.",
+            });
+            return;
+        }
+
+        try {
+            const response = await CifrarServices.convertAndDecrypt(fileContent, key, delimiter);
+            //console.log('Respuesta del servicio:', response.data);
+            navigate("/dResultado", { state: { textData: response.data } });
+        } catch (error) {
+            console.error('Error en la conversión y desencriptación:', error);
+            MySwal.fire({
+                icon: "error",
+                title: "Error",
+                text: "Ocurrió un error al convertir y desencriptar los datos.",
+            });
+        }
+    };
+
     const handleBack = () => {
-        navigate('/');
+        navigate("/");
     };
-    
-    return(
+
+    return (
         <div className="max-w-md mx-auto mt-36 md:mt-40 lg:mt-40 md:max-w-xl p-6 bg-cardfont border rounded-lg shadow-md">
-            <button className=" border-solid border-2 focus:outline-none rounded-md p-2 md:text-lg bg-yellow-300 hover:bg-yellow-400 w-full font-medium">FileChooser</button>
-                <div className="mx-auto m-5 ">
-                    <label className="px-3 md:text-lg">Ingrese la clave:</label>
-                    <input type="text" placeholder="Ingrese su clave..." className="p-1 rounded-md md:text-lg" required/>
-                </div>
-                <div className="mx-auto m-5">
-                    <label className=" px-6 md:text-lg">Delimitador:</label>
-                    <input type="text" placeholder="Ingrese el delimitador..." className="p-1 rounded-md md:text-lg" required/>
-                </div>
-                <div className="flex mx-auto gap-3 justify-center">
-                    <button onClick={handleBack} className="bg-red-800 hover:bg-pink-700 text-white font-semibold p-2 md:text-lg rounded focus:outline-none">
-                        Regresar</button>
-                    <button onClick={handleDecrypt} className="bg-green hover:bg-emerald-600 text-white font-semibold p-2 md:text-lg rounded focus:outline-none">
-                        Desencriptar</button>
-                </div>
+            <div className="flex flex-col sm:flex-row justify-between mb-5">
+                <label className="flex items-center justify-center bg-yellow-500 p-2 rounded-lg cursor-pointer mb-2 sm:mb-0 w-full h-full">
+                    <span className="font-bold text-white text-lg">FileChooser</span>
+                    <input
+                        type="file"
+                        accept=".json"
+                        className="hidden"
+                        onChange={handleFileUpload}
+                    />
+                </label>
+            </div>
+
+            <div className="mx-auto m-5">
+                <label className="px-3 md:text-lg">Ingrese la clave:</label>
+                <input
+                    type="text"
+                    placeholder="Ingrese su clave..."
+                    className="p-1 rounded-md md:text-lg"
+                    value={key}
+                    onChange={(e) => setKey(e.target.value)}
+                    required
+                />
+            </div>
+
+            <div className="mx-auto m-5">
+                <label className="px-6 md:text-lg">Delimitador:</label>
+                <input
+                    type="text"
+                    placeholder="Ingrese el delimitador..."
+                    className="p-1 rounded-md md:text-lg"
+                    value={delimiter}
+                    onChange={(e) => setDelimiter(e.target.value)}
+                    required
+                />
+            </div>
+
+            <div className="flex mx-auto gap-3 justify-center">
+                <button onClick={handleBack} className="bg-red-800 hover:bg-pink-700 text-white font-semibold p-2 md:text-lg rounded focus:outline-none">
+                    Regresar
+                </button>
+                <button onClick={handleDecrypt} className="bg-green hover:bg-emerald-600 text-white font-semibold p-2 md:text-lg rounded focus:outline-none">
+                    Desencriptar
+                </button>
+            </div>
         </div>
     );
-}
+};
+
 export default DecryptCard;
